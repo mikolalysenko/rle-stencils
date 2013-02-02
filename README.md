6a438bf9b95bf96cb971548fd517378502ca1ca9
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-02 09:45:54 -0600

    changed stencil format to a flattened typed array

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 rle-stencils
 ============
-
 Stencil generator code for the rle libraries
 
 
--- stencils.js
+++ stencils.js
@@ -5,11 +5,13 @@ function moore(radius_) {
   for(var i=-radius; i<=radius; ++i) {
     for(var j=-radius; j<=radius; ++j) {
       for(var k=-radius; k<=radius; ++k) {
-        result.push([i,j,k]);
+        result.push(i);
+        result.push(j);
+        result.push(k);
       }
     }
   }
-  return result;
+  return new Int32Array(result);
 }
 
 //Creates an Lp ball stencil
@@ -18,13 +20,15 @@ function lp(p, radius_) {
     return moore(radius_);
   }
   var result = []
-    , radius = Math.ceil(radius_)
+    , radius = Math.ceil(radius_)|0
     , rp     = Math.pow(radius_, p);
   for(var i=-radius; i<=radius; ++i) {
     for(var j=-radius; j<=radius; ++j) {
       for(var k=-radius; k<=radius; ++k) {
         if(Math.pow(Math.abs(i), p) + Math.pow(Math.abs(i), p) + Math.pow(Math.abs(i), p) <= rp) {
-          result.push([i,j,k]);
+          result.push(i);
+          result.push(j);
+          result.push(k);
         }
       }
     }
@@ -34,11 +38,9 @@ function lp(p, radius_) {
 
 
 //Special stencils for 
-var CROSS_STENCIL       = [ [0,0,0] ]
-  , CUBE_STENCIL        = [ ];
-
+var CUBE_STENCIL;
 (function() {
-  //Build surface stencil
+  var cube = []
   for(var i=0; i<8; ++i) {
     var p = [0,0,0];
     for(var j=0; j<3; ++j) {
@@ -46,25 +48,30 @@ var CROSS_STENCIL       = [ [0,0,0] ]
         p[j] = -1;
       }
     }
-    CUBE_STENCIL.push(p);
+    cube.push(p[0]);
+    cube.push(p[1]);
+    cube.push(p[2]);
   }
+  CUBE_STENCIL = new Int32Array(cube);
 })();
 
+var CROSS_STENCIL;
 (function() {
-  //Build cross
+  var cross = [0,0,0];
   for(var i=0; i<3; ++i) {
     for(var s=-1; s<=1; s+=2) {
-      var p = [0,0,0];
-      p[i] = s;
-      CROSS_STENCIL.push(p);
+      cross.push(i === 0 ? s : 0);
+      cross.push(i === 1 ? s : 0);
+      cross.push(i === 2 ? s : 0);
     }
   }
+  CROSS_STENCIL = new Int32Array(cross);
 })();
 
 //Export stencils
 exports.lp            = lp;
 exports.moore         = moore;
-exports.vonNeumann    = lp.bind(this, 1);
-exports.ball          = lp.bind(this, 2);
+exports.vonNeumann    = lp.bind(null, 1);
+exports.ball          = lp.bind(null, 2);
 exports.CROSS_STENCIL = CROSS_STENCIL;
 exports.CUBE_STENCIL  = CUBE_STENCIL;
